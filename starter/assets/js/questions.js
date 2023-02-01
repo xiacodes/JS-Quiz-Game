473f215c84ff5b9f8f8e77c710f818c2f944fa50
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-02-01 11:36:43 +0000

    Working on the viewCurrentQuestion() function

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,27 +1,27 @@
 // The question objects
 var questions = [
 	{
-		question: "What is CSS used for?",
+		title: "What is CSS used for?",
 		answers: ["Writing JavaScript code", "Creating a website layout and design", "Storing website data", "Processing user input"],
 		correct: "Creating a website layout and design",
 	},
 	{
-		question: "How do you select an element with the ID 'example' in CSS?",
+		title: "How do you select an element with the ID 'example' in CSS?",
 		answers: ["<example> { }", ".example { }", "example { }", "#example { }"],
 		correct: "#example { }",
 	},
 	{
-		question: "How do you add JavaScript to an HTML file?",
+		title: "How do you add JavaScript to an HTML file?",
 		answers: ["Storing website data", "By using the <style> tag in the <body> section", "By using the <script> tag in the <head> section", "By using the <jscript> tag in the <header> section"],
 		correct: "By using the <script> tag in the <head> section",
 	},
 	{
-		question: "How do you make a function in JavaScript that displays an alert?",
+		title: "How do you make a function in JavaScript that displays an alert?",
 		answers: ["function alertFunction() { alert('Hello!'); }", "function displayAlert() { alertBox(Hello!''); }", "function showMessage() { alert('Hello!''); }", "function popUp() { alert('Hello!''); }"],
 		correct: "function alertFunction() { alert('Hello!'); }",
 	},
 	{
-		question: "How do you set the background color of a page to yellow in CSS?",
+		title: "How do you set the background color of a page to yellow in CSS?",
 		answers: ["body {color: yellow;}", "body {background-color: yellow;}", "page {color: yellow;}", "page {background: yellow;}"],
 		correct: "body {background-color: yellow;}",
 	},
@@ -40,4 +40,8 @@ var answers = [answer1, answer2, answer3, answer4];
 // Loop and add the li elements to the ol element
 for (let index = 0; index < answers.length; index++) {
 	choicesList.appendChild(index);
+}
+
+function viewCurrentQuestion() {
+	questionTitle.textContent = questions[currentQuestion].title;
 }
